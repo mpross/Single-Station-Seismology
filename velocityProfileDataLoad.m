b2e1eba92d184ccb2d27c55e356ac19c65cbff08
M.P.Ross <mpross2@uw.edu>
2018-10-12 16:45:42 -0700

    Started documenting the analysis.

---
+++ velocityProfileDataLoad.m
@@ -0,0 +1,84 @@
+%% Set-up
+% close all
+
+earthquakes=["Mexico_5_9" "Oklahoma_4_4" "Indonesia_6_9" "Fiji_8_2" "CostaRica_6_1" ...
+    "Fiji_6_8" "Oregon_6_2" "Venezuela_7_3" "Peru_7_1" "Fiji_7_8" "NewZealand_6_9"];
+timeStamp=[1214366228 1212587999 1218725806 1218673195 1218583362 ...
+    1218688157 1218965525 1218922324 1219136664 1220284172 1220588360];
+
+vel=[];
+vFreq=[];
+sampF=8;
+
+%% Data pull and decimate
+j=1;
+earthquakes(j)
+filename=strcat('/home/michael/Google Drive/Seismology/Data/GPS',num2str(timeStamp(j)),'_',earthquakes(j),'.mat');
+
+rawData=load(filename);
+
+rawBRSX=rawData.rawData(1);
+inBRSX=decimate(rawBRSX.data,rawBRSX.rate/8)*1e-9;
+
+rawBRSY=rawData.rawData(2);
+inBRSY=decimate(rawBRSY.data,rawBRSY.rate/8)*1e-9;
+
+rawSTSX=rawData.rawData(3); 
+inSTSX=decimate(rawSTSX.data,rawSTSX.rate/8)*1e-9;
+
+rawSTSY=rawData.rawData(4); 
+inSTSY=decimate(rawSTSY.data,rawSTSY.rate/8)*1e-9;
+
+rawSTSZ=rawData.rawData(5); 
+inSTSZ=decimate(rawSTSZ.data,rawSTSZ.rate/8)*1e-9;
+
+inBRSX=inBRSX(1:1e5);
+inBRSY=inBRSY(1:1e5);
+inSTSX=inSTSX(1:1e5);
+inSTSY=inSTSY(1:1e5);
+inSTSZ=inSTSZ(1:1e5);
+
+%% Time cut
+timeThreshold=3e-6;
+
+timeCut=find(abs(inSTSZ-mean(inSTSZ))>timeThreshold);
+startTime=(timeCut(1)-1000);
+
+timeCut=find(abs(fliplr(inSTSZ')-mean(inSTSZ))>timeThreshold);
+endTime=(length(inSTSZ)-(timeCut(1)-2000));
+if(startTime<0)
+    startTime=1;
+end
+
+if(endTime>length(inSTSZ))
+    endTime=length(inSTSZ);
+end
+
+%% Inversion and filtering
+
+STSInvertFilt = zpk(-2*pi*[pairQ(8.33e-3,0.707)],-2*pi*[0 0],1);
+STSInvertFilt = 1*STSInvertFilt/abs(freqresp(STSInvertFilt,2*pi*100));
+
+time=(startTime:endTime)/sampF;
+
+[b,a]=butter(3,0.01*2/sampF,'high');
+
+BRSX=inBRSX(startTime:endTime);
+BRSY=inBRSY(startTime:endTime);
+
+STSX=lsim(STSInvertFilt,inSTSX(startTime:endTime), time-startTime/sampF);
+STSY=lsim(STSInvertFilt,inSTSY(startTime:endTime), time-startTime/sampF);
+STSZ=lsim(STSInvertFilt,inSTSZ(startTime:endTime), time-startTime/sampF);
+
+STSX=filter(b,a,STSX);
+STSY=filter(b,a,STSY);
+STSZ=filter(b,a,STSZ);
+BRSX=filter(b,a,BRSX);
+BRSY=filter(b,a,BRSY);
+
+STSX=STSX(250*sampF:end);
+STSY=STSY(250*sampF:end);
+STSZ=STSZ(250*sampF:end);
+BRSX=BRSX(250*sampF:end);
+BRSY=BRSY(250*sampF:end);
+time=time(250*sampF:end);
